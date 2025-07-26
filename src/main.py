9c3e13aa2cfd8eb660eee2f94a0b9d8c7fe507da
Afnan Attar <afnan.attar47@gmail.com>
2025-07-26 12:51:19 +0530

    changed layout to wide

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 st.set_page_config(
     page_title="Cloud Cost Calculator",
     page_icon="💡",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="expanded"
 )
 
